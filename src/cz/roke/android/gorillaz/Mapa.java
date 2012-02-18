45722051dc0174fe7c5a362c9fc1912487cd88c1
unknown <redhat@redbook.(none)>
2012-02-18 15:48:24 +0100

    bleeee

--- src/cz/roke/android/gorillaz/Mapa.java
+++ src/cz/roke/android/gorillaz/Mapa.java
@@ -9,14 +9,16 @@ import android.graphics.Paint;
 public class Mapa {
 
 	public Bitmap collisionMap;
+	public Bitmap pozadi;
 	
-	public Mapa(Resources resources, int collisionMapId) {
+	public Mapa(Resources resources, int collisionMapId, int pozadiId) {
 		collisionMap = BitmapFactory.decodeResource(resources, collisionMapId);
+		pozadi = BitmapFactory.decodeResource(resources, pozadiId);
 		
 	}
 	
 	public void draw(Canvas canvas, Paint paint) {
-		canvas.drawBitmap(collisionMap, 0, 0, paint);
+		canvas.drawBitmap(pozadi, 0, 0, paint);
 	}
 	
 }
